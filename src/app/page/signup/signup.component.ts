3e0045c85de023926d3dda1fe8091b4dd190c102
himanshu <himanshu.raj@byg.co.in>
2018-10-28 15:58:11 +0530

    dpone signuop pages

--- src/app/page/signup/signup.component.ts
+++ src/app/page/signup/signup.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  selectedOption : String = "";
+
+  bloodGroupList = ["O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-"];
+
+  getTodaysDate(){
+    let today : any = new Date();
+    let dd : any = today.getDate();
+    let mm : any = today.getMonth()+1; //January is 0!
+    let yyyy : any = today.getFullYear();
+    if(dd<10)
+            dd='0'+dd
+        if(mm<10)
+            mm='0'+mm
+    today = yyyy+'-'+mm+'-'+dd;
+    return today;
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectOption(event){
+    this.selectedOption = event.target.value;
   }
 
 }
